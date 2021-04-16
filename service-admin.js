4bbbb52a7f17992f05bbaff03cd003a255204068
Cisco Caceres <cisco801@gmail.com>
2021-04-15 22:26:37 -0400

    Update WIP

--- service-admin.js
+++ service-admin.js
@@ -12,6 +12,11 @@ logger.info('Startup', SCRIPT_INFO);
 
 const authMain = require('./auth-main');
 
+const https = require('https');
+const axios = require('axios');
+
+const axiosOptions = {};
+
 const fastify = require('fastify')({
     logger: false,
     //http2: true,
@@ -25,19 +30,20 @@ fastify.register(require('fastify-static'), {
     prefix: '/',
 })
 
+const HOSTS = ['i.dev.fortifid.com', 'i.prod.fortifid.com', 'z.prod.fortifid.com'];
+
 const pm2 = require('pm2');
 
 const awsClient = require('./aws-client');
 
 let pm2Connected = false;
 
-const execCommand = async (file, args)=> {
+const execCommand = async (file, args) => {
     try {
-        const data = {
-        };
+        const data = {};
 
-        if(SCRIPT_INFO.host) {
-            data.host = SCRIPT_INFO.host     
+        if (SCRIPT_INFO.host) {
+            data.host = SCRIPT_INFO.host
         }
 
         data.start = Date.now();
@@ -46,26 +52,80 @@ const execCommand = async (file, args)=> {
             stdout,
             stderr
         } = await utils.execFile(file, args);
-        
+
         data.end = Date.now();
         data.duration = data.end - data.start;
 
         let temp = utils.splitLines(stdout);
-        if(temp) {
-            data.output = temp; 
+        if (temp) {
+            data.output = temp;
         }
-        
-        temp = utils.splitLines(stderr);
-        if(temp) {
-            data.error = temp;
-        } 
 
-        return data; 
+        temp = utils.splitLines(stderr);
+        if (temp) {
+            data.error = temp;
+        }
+
+        return data;
     } catch (error) {
         logger.error(error);
-        return {error: error.message};
+        return {
+            error: error.message
+        };
     }
 }
+
+const sendCommand = async (url, method, data) => {
+    const options = {
+        method,
+        url,
+        //headers: headers,
+        //data,
+        timeout: 10000,
+        maxContentLength: Infinity,
+
+    };
+
+    if (axiosOptions.httpsAgent) {
+        options.httpsAgent = axiosOptions.httpsAgent;
+    }
+
+    try {
+        const result = await axios(options);
+        return result.data;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+const sendHosts = async (hosts, endpoint, method, data) => {
+    if (!axiosOptions.httpsAgent) {
+        return;
+    }
+
+    const results = {};
+
+    if (SCRIPT_INFO.host) {
+        results.host = SCRIPT_INFO.host
+    }
+
+    const funcs = [];
+    hosts.forEach(host => {
+        funcs.push(sendCommand(`https://${host}${endpoint}`, method, data));
+    });
+
+    try {
+        results.start = Date.now();
+        results.output = await Promise.all(funcs);
+    } catch (error) {
+        results.error = error.message;
+    }
+    results.end = Date.now();
+    results.duration = results.end - results.start;
+
+    return results;
+}
+
 
 const update = async (args) => {
     return await execCommand(`${__dirname}/data/update.sh`, args);
@@ -87,7 +147,7 @@ const restart = () => {
     }
 }
 
-const importAccount = ()=> {
+const importAccount = () => {
     // let id = 'A99E264A82E3F746123C68098F2D116B6E483F289FB77DF3725314EFD30D258F';
     // let data = await getAuthz(id);
     // await utils.fileWrite( `./tmp/${id}.json`, JSON.stringify(data, null, 2));
@@ -97,50 +157,28 @@ const importAccount = ()=> {
 
 fastify.get('/update', async (request, reply) => {
     let results = await update(['all']);
-    
+
     reply.type('application/json').code(200).send(results);
     restart();
 })
 
 fastify.get('/push-updates', async (request, reply) => {
-    let results = await pushUpdates();
-    
+    let results = await sendHosts(HOSTS, '/admin/v1/update');
+
     reply.type('application/json').code(200).send(results);
 })
 
-// //TODO: Extract these to separate files
-// async function handleSystem(action, bodyData, key) {
-//     if (key !== PARAMS.system_secret) {
-//         utils.sendData(res, 'Invalid key', 401);
-//         return;
-//     }
+fastify.get('/info', async (request, reply) => {
+    const now = Date.now();
+    const data = {
+        ...SCRIPT_INFO,
+        time: now,
+        uptime: Math.round(process.uptime()),
+    };
+    
+    reply.type('application/json').code(200).send(data);
+})
 
-//     switch (action) {
-//         case 'restart': {
-//             utils.sendData(res, 'OK');
-//             restart();
-//             break;
-//         }
-//         case 'update': {
-//             utils.sendData(res, 'OK');
-//             update();
-//             break;
-//         }
-//         case 'server': {
-//             const data = {
-//                 ...SCRIPT_INFO,
-//                 start: utils.startTime,
-//                 time: now,
-//                 uptime: (now - utils.startTime),
-//             };
-//             utils.sendData(res, data);
-//             break;
-//         }
-//         default: {
-//             utils.sendData(res, 'Not found.', 404);
-//         }
-//     }
-// }
 
 fastify.addHook("onRequest", async (request, reply) => {
     if (!await authMain.checkHeaders(request, reply, 0, true)) {
@@ -154,6 +192,23 @@ fastify.listen(9999, (err, address) => {
 });
 
 (async () => {
+    if (process.env.CLIENT_CERT && await utils.fileExists(process.env.CLIENT_CERT) &&
+        process.env.CLIENT_KEY && await utils.fileExists(process.env.CLIENT_KEY)) {
+        try {
+            const httpsAgent = new https.Agent({
+                cert: await utils.fileRead(process.env.CLIENT_CERT),
+                key: await utils.fileRead(process.env.CLIENT_KEY),
+                //  ca: fs.readFileSync('ca.crt'),
+            });
+
+            if (httpsAgent) {
+                axiosOptions.httpsAgent = httpsAgent
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     //await loadParams();
     try {
         pm2.connect((err) => {
