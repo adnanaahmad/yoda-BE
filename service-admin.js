61f232434466d4d474104f3dc598b13d3201e34c
Cisco Caceres <cisco801@gmail.com>
2021-05-03 23:11:39 -0400

    Admin service enhancements.

--- service-admin.js
+++ service-admin.js
@@ -223,11 +223,13 @@ const trim = async () => {
             for (let index = 0; index < count - 5 ; index++) {
                 const file = files[index];
                 try {
-                    await utils.fileDelete(`${dir}/${file}`)
+                    //await utils.fileDelete(`${dir}/${file}`)
+                    deleted.push(`${dir}/${file}`);
                 } catch (error) {
-                                    
+                    console.log(error);          
                 }
-                deleted.push(file);
+
+                //deleted.push(file);
             }
 
             data = {deleted: deleted};
