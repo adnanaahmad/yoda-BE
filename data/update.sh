74868b6272002a63e2bd8b0e6a77c8bc654b2419
Cisco Caceres <cisco801@gmail.com>
2021-05-03 21:26:00 -0400

    Admin service enhancements.

--- data/update.sh
+++ data/update.sh
@@ -34,11 +34,11 @@ if [ -d ./fortifid -a ! -h ./fortifid ]; then
     then 
         log "Installing..."
         tar -zxf didservice.tar.gz --directory fortifid
-        log "Backing up archive..."
         #rm -rf didservice.tar.gz
         mkdir -p ./backups
         #hash=`sha224sum didservice.tar.gz | awk '{ print $1 }'`
         version=awk -F'"' '/"version": ".+"/{ print $4; exit; }' ./fortifid/package.json
+        log "Backing up archive ($version)..."
         mv didservice.tar.gz "./backups/$version.tar.gz"
         #TODO: actually check for success
         cd fortifid
