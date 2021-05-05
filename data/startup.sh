e53982aa73d755614052fa323c722128ae2d7829
Cisco Caceres <cisco801@gmail.com>
2021-05-05 17:42:11 -0400

    Startup script.

--- data/startup.sh
+++ data/startup.sh
@@ -2,4 +2,5 @@
 
 echo "$(date)" >> /home/ec2-user/reboots
 
-sudo -u ec2-user bash -c "/home/ec2-user/fortifid/data/update.sh reload >/home/ec2-user/last-update.txt"
+#sudo -u ec2-user bash -c "/home/ec2-user/fortifid/data/update.sh reload >/home/ec2-user/last-update.txt"
+/home/ec2-user/fortifid/data/update.sh reload >/home/ec2-user/last-update.txt
