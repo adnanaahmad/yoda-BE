d880a189ebd0184ba7297dabb6a9d2c9f4765b44
Cisco Caceres <cisco801@gmail.com>
2021-05-08 14:01:33 -0400

    Minor fix.

--- scripts/trim.sh
+++ scripts/trim.sh
@@ -7,5 +7,12 @@ if [ ! -d "$BACKUP_DIR" ]; then
     exit 1
 fi
 
-rm "$BACKUP_DIR/(ls $BACKUP_DIR -t | awk 'NR>5')"
+cd $BACKUP_DIR
+if [ "$(pwd)" != "$BACKUP_DIR" ]; then
+    echo "Unable to switch to $BACKUP_DIR. Cannot continue."
+    exit 1
+fi
+
+ls -t | tail -n +4 | xargs rm --
+
 echo 'Done.'
