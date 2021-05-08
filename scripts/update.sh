57d3815b14967d596aed7bc6351c9c04c6b7bb17
Cisco Caceres <cisco801@gmail.com>
2021-05-08 13:37:45 -0400

    Minor fix.

--- scripts/update.sh
+++ scripts/update.sh
@@ -83,7 +83,9 @@ fi
 version=`awk -F'"' '/"version": ".+"/{ print $4; exit; }' ./fortifid/package.json`
 old_version=`awk -F'"' '/"version": ".+"/{ print $4; exit; }' ./fortifid/package.json.old` 
 
-if $version = $old_version ; then
+log "$version = $old_version" 
+
+if [ "$version" == "$old_version" ]; then
     log "Version is the same as previous version." 
     exit 1
 fi 
