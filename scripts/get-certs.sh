6832a1203cbc572c6d70b2391c83b27f8ad75be6
Cisco Caceres <cisco801@gmail.com>
2021-05-10 17:16:12 -0400

    Setup related.

--- scripts/get-certs.sh
+++ scripts/get-certs.sh
@@ -36,7 +36,7 @@ log "Copying latest chain.pem from the parameter store."
 aws ssm get-parameter --name "/config/apigw/client/chain.pem" > /etc/nginx/ssl/chain.pem --with-decryption --output text --query Parameter.Value
 
 if sudo test -f "$CHAIN" ; then
-    if sudo /usr/local/bin/certbot renew > /dev/null ; then
+    if sudo /usr/local/bin/certbot renew ; then
         if copy ; then
            start_nginx
         fi
