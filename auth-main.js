d05e7fc4c6975417050529063be3608948f6d027
Cisco Caceres <cisco801@gmail.com>
2021-11-26 02:28:59 -0500

    Webhook.

--- auth-main.js
+++ auth-main.js
@@ -61,9 +61,8 @@ const sendWebhook = async (customer_id, data, table, handler) => {
             if (customer.webhooks) {
                 const webhook = customer.webhooks[table];
                 if (webhook && webhook.url && webhook.url.startsWith("https")) {
-                    const payload = { data: data, url: webhook.url };
+                    const payload = { data: JSON.stringify(data), url: webhook.url };
                     if (webhook.secret && webhook.secret.length > 0) {
-                        payload.data = JSON.stringify(data);
                         const sig = utils.hash(`${payload.data}${webhook.secret}`, 'sha256', 'hex').toUpperCase();
                         payload.headers = { "x-signature": sig ,  "x-api": table };
                     }
