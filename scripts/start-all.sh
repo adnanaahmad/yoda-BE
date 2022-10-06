5b9e2acd372ef07bddc55885d95bdbd4e73b85f0
Cisco Caceres <cisco801@gmail.com>
2022-10-05 22:58:52 -0400

    Minor startup change.

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -4,7 +4,7 @@ if [ -z "$SHARED_LOADED" ]; then
     . "/home/ec2-user/fortifid/scripts/shared.sh"
 fi
 
-START=service-admin.js service-certs.js service-did.js service-mfa.js service-plaid-ach.js service-synthetic-id.js service-veriff.js handler-email.js handler-twilio.js handler-webhook.js helper-forwarder.js helper-shortener.js helper-uploader.js
+START="service-admin.js service-certs.js service-mfa.js service-plaid-ach.js service-synthetic-id.js service-veriff.js handler-email.js handler-twilio.js handler-webhook.js helper-forwarder.js helper-shortener.js helper-uploader.js"
 
 cd $FORTIFID_DIR
 if [ "$(pwd)" != "$FORTIFID_DIR" ]; then
@@ -18,6 +18,6 @@ if [ -n "$START" ]; then
     #     pm2 start "$i.js"
     # done
     pm2 start $START -i max --exp-backoff-restart-delay=100
-    pm2 start helper-scheduler.js --exp-backoff-restart-delay=100
+    pm2 start helper-scheduler.js service-did.js --exp-backoff-restart-delay=100
     pm2 save
 fi
