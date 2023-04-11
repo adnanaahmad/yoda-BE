2b418fe346cf1cdb6475b76174737ee0a78ebbaa
Adnan Ahmad <adnanahmad.khayal@gmail.com>
2023-04-12 01:03:00 +0500

    crypt annotated

--- crypt.js
+++ crypt.js
@@ -6,10 +6,11 @@ const crypto = require('crypto');
 let params;
 let DEFAULT_KEY;
 
+//This code defines a function getRandomKey that returns a random key of specified len (default 32 bytes) generated using the crypto.randomBytes method.
 const getRandomKey = (len = 32) => {
     return crypto.randomBytes(len);
 }
-
+// This code defines a function encrypt that takes plaintext data, an encryption key (or uses a default key), and an optional algorithm, and returns an object containing the encrypted data, an initialization vector iv, and the algorithm used for encryption, with the plaintext data encrypted using the specified algorithm and the provided key.
 const encrypt = (data, key, algorithm = 'aes-256-cbc') => {
     key = key || DEFAULT_KEY;
     const iv = crypto.randomBytes(16);
@@ -27,7 +28,7 @@ const encrypt = (data, key, algorithm = 'aes-256-cbc') => {
         data: encrypted.toString('base64')
     };
 }
-
+// This code defines a function decrypt that takes encrypted data data and a decryption key, and returns the decrypted plaintext data, using the initialization vector and algorithm contained in the encrypted data and verifying the key. It also checks if the decrypted data is in JSON format, and returns the parsed object if it is.
 const decrypt = (data, key) => {
 
     key = key || DEFAULT_KEY;
@@ -54,7 +55,7 @@ const decrypt = (data, key) => {
 
     return decrypted;
 }
-
+// This code initializes the DEFAULT_KEY global variable by retrieving a base64-encoded key from a configuration file, then logs a message to the console indicating that the crypt has been initialized.
 (async () => {
     try {
         params = await require('./params')('/config/shared/crypt', undefined, false, true);
